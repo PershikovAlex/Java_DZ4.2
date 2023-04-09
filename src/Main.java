409f242c3321042a5a55438a2cf53e40d089c526
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-04-09 10:40:13 +0500

    Add calculate to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+    BmiService service = new BmiService();
+
+    System.out.println("Индекс массы тела: " + service.calculate(98, 1.87));
+
+    System.out.println("Индекс массы тела: " + service.calculate(100, 1.67));
+
+    System.out.println("Индекс массы тела: " + service.calculate(75, 1.99));
+    }
 }
